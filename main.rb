7fd4672c265e3a9b515bbca6008fb90550a00aeb
Vitaliy <vitaly.beskrovny@gmail.com>
2015-08-28 16:25:39 +0300

    * replace lattitude and longitude

--- main.rb
+++ main.rb
@@ -2,14 +2,14 @@ require 'bundler/setup'
 
 require_relative './map'
 
-map_image = Map.new('images/map.jpg', [-50.0, -100.0], [60.0, 180.0])
+map_image = Map.new('images/map.jpg', [-180.0, 90.0], [180.0, -90.0])
 
 points = [
-  [51.5085300, -0.1257400],   # London 
-  [40.71427, -74.0059700],    # New York 
-  [55.7522200, 37.6155600],   # Moscow 
-  [-33.8678500, 151.2073200], # Sydney
-  [-34.6131500, -58.3772300]  # Buenos Aires
+  [-0.1257400, 51.5085300],   # London 
+  [-74.0059700, 40.71427],    # New York 
+  [37.6155600, 55.7522200],   # Moscow 
+  [151.2073200, -33.8678500], # Sydney
+  [-58.3772300, -34.6131500]  # Buenos Aires
 ]
 
 map_image.add_points(points)
